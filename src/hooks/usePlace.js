30fad8f4a228a1b237c8719ea8d2b95bbe95d514
Pär Thernström <par.thernstrom@gmail.com>
2020-03-21 22:44:51 +0100

    Add hook usePlace

---
+++ src/hooks/usePlace.js
@@ -0,0 +1,9 @@
+import { useAllPlaces } from "./useAllPlaces";
+
+export const usePlace = (path) => {
+  const allPlaces = useAllPlaces();
+  const place = allPlaces.find(place => {
+    return place.path = path
+  })
+  return place;
+};
