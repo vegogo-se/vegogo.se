183e3a9a2bfa6861477a4c17736ef990f9388a8a
Pär Thernström <par.thernstrom@gmail.com>
2020-03-28 16:37:05 +0100

    Add purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,18 @@ module.exports = {
       }
     },
     "gatsby-plugin-bundle-stats",
-    "gatsby-plugin-postcss"
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        tailwind: true // Enable tailwindcss support
+        // develop: true, // Enable while using `gatsby develop`
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    }
     // "gatsby-plugin-webpack-bundle-analyser-v2"
   ]
 };
