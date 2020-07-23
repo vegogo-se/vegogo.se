53a49efeacb2171f04feec69f47f9b8625a02c4d
Pär Thernström <par.thernstrom@gmail.com>
2020-07-23 21:36:16 +0200

    Add comment to why "esm" is used in gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,2 +1,4 @@
+// Make ES6 imports work by using esm.
+// Solution from https://github.com/gatsbyjs/gatsby/issues/7810#issuecomment-449741977.
 require = require("esm")(module);
 module.exports = require("./gatsby-node.esm.js");
